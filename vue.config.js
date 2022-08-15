16d5b99d3e24077778e0001d5da1a5eb9335289f
Hedgrin <eugen.dzubenko@onix-systems.com>
2022-08-15 16:31:41 +0300

    added spa prereneder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
+
+const plugins = [];
+
+if (process.env.NODE_ENV === "production") {
+  const { join } = require("path");
+  const PrerenderPlugin = require("prerender-spa-plugin-next");
+
+  plugins.unshift(
+    new PrerenderPlugin({
+      staticDir: join(__dirname, "dist"),
+      routes: ["/"],
+      renderAfterDocumentEvent: "render-event",
+    })
+  );
+}
+
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
@@ -19,5 +35,8 @@ module.exports = defineConfig({
          `,
       },
     },
+  },
+  configureWebpack(config) {
+    config.plugins = [...config.plugins, ...plugins];
   },
 });
